a48a1dfce4e39e3bb8ca716e8dc823974584399e
Askar Kanturin <a.kanturin@yandex.ru>
2023-07-25 17:20:06 +0300

    did some things
    things were in fact done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,31 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    assets: true,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // path to your scss variables
+          additionalData: `@import "@/styles/pallete.scss";`
+        }
+      }
+    }
+  }
+});
+
+
+
