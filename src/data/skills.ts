bb416e7726c1860d050ec1cdf24353ec989dae2d
Askar Kanturin <a.kanturin@yandex.ru>
2023-07-27 20:24:04 +0300

    added sick tabs
    also started filling out my skills

---
+++ src/data/skills.ts
@@ -0,0 +1,61 @@
+const skills: {title: string, description: string}[] = [
+	{
+		title: "HTML",
+		description: `
+С изучения HTML начался мой путь в веб-разработке и программировании в целом.
+<br/>
+Хоть HTML не язык программирования, а разметки, но для многих (включая меня) 
+именно от него люди потом переходят к языкам программирования. 
+<br/>
+Несмотря на то, что язык старый <i>(первая версия HTML была опубликована в 1993 году)</i>, 
+в него регулярно добавляют новые элементы, которые делают 
+веб-разработку проще, мощнее или и то, и то сразу.
+<br/><br/>
+В веб-разработке я естественно регулярно использую HTML, 
+стараюсь использовать семантические теги когда возможно, 
+и в целом писать код так, чтобы его легче всего было понять в будущем.
+`,
+	},	
+	{
+		title: "CSS/SCSS",
+		description: ``
+	},	
+	{
+		title: "JS/TS",
+		description: ``
+	},
+	{
+		title: "Wordpress",
+		description: ``
+	},
+	{
+		title: "React",
+		description: ``
+	},
+	{
+		title: "Nest.js",
+		description: ``
+	},
+	{
+		title: "Astro",
+		description: ``
+	},
+	{
+		title: "Haxe",
+		description: ``
+	},
+	{
+		title: "Dart",
+		description: ``
+	},
+	{
+		title: "C",
+		description: ``
+	},
+	{
+		title: "Godot",
+		description: ``
+	},
+];
+
+export default skills;
